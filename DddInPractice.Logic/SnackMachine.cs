54b63003e02debf242f0b72f823ef3361e05d312
kiran.gondi <kiran.gondi@planview.com>
2023-11-27 16:06:19 +0530

    After chapter 2 completed

---
+++ DddInPractice.Logic/SnackMachine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using static DddInPractice.Logic.Money;
+namespace DddInPractice.Logic
+{
+  //DDD: Entity
+  public sealed class SnackMachine : Entity
+  {
+    public Money MoneyInside { get; private set; }
+    public Money MoneyInTransaction { get; private set; }
+
+    //public SnackMachine()
+    //{
+    //  MoneyInside = None;
+    //  MoneyInTransaction = None;
+    //}
+    public void InsertMoney(Money money)
+    {
+      Money[] coinsAndNotes = { Cent, TenCent, Quarter, Dollar, FiveDollar, TwentyDollar };
+      if (!coinsAndNotes.Contains(money))
+        throw new InvalidOperationException();
+
+      MoneyInTransaction += money;
+    }
+
+    public void ReturnMoney()
+    {
+      MoneyInTransaction = None;
+    }
+
+    public void BuySnack()
+    {
+      MoneyInside += MoneyInTransaction;
+      MoneyInTransaction = None;
+    }
+
+  }
+}
